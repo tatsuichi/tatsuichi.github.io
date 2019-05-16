cff16047b5b32e0c66274f9012ee74a6cef7e466
tatsuichi <tatsuri1984@gmail.com>
2019-05-16 23:18:51 +0900

    上野→大宮間の路線が遅延していたらアラートで通知する

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-  alert("Hello");
-  
+
   const xhr = new XMLHttpRequest();
-//  xhr.open("GET", `https://rti-giken.jp/fhc/api/train_tetsudo/delay.json`, true);
-  xhr.open("GET", `https://www.yahoo.co.jp/`, true);
-//  xhr.responseType = 'json';
+  xhr.open("GET", `https://rti-giken.jp/fhc/api/train_tetsudo/delay.json`, true);
+  xhr.responseType = 'json';
   xhr.onreadystatechange = function() {
-    if(xhr.readyState === 0) {	// 操作が完了した
+    if(xhr.readyState === 4) {	// 操作が完了した
       if(xhr.status === 200) {	// OK
-        console.log(xhr.response);
         
-        var json = JSON.parse(xhr.response);
+        var json = xhr.response;
+        for (var i=0; i<json.length; i++) {
+          if(json[i].company === "JR東日本"){
+            if(json[i].name === "宇都宮線" || json[i].name === "高崎線"){
+              alert("json[i].name");
+            }
+            if(json[i].name === "京浜東北線"){
+              alert("json[i].name");
+            }
+            if(json[i].name === "湘南新宿ライン" || json[i].name == "東海道線"){
+              alert("json[i].name");
+            }
+          }
+        }
         
-        var company = JR東日本;
-        var name1 = 山手線;
-        var name2 = 湘南新宿ライン;
-        var name3 = 東海道線;
-        var name4 = 宇都宮線;
-        var name5 = 高崎線;
-        var name6 = 京浜東北線;
-      } else {
-        console.log(xhr.status);
       }
-    } else {
-      console.log(xhr.readyState);
     }
   };
   xhr.send();
